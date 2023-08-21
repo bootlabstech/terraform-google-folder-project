86f2dcfc17b27b2bc3fc94efad9ce248dd9cb916
Silpashree Acharya <silpashree@bootlabstech.com>
2023-08-21 12:03:21 +0530

    edited output

--- outputs.tf
+++ outputs.tf
@@ -5,6 +5,3 @@ output "id" {
 output "project_id" {
   value = trimprefix(google_project.my_project.id, "projects/")
 }
-output "project_number" {
-  value = data.google_project.my_project.project_number
-}
